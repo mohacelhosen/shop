a2f0ba99ae9877f685604965f55117817a6fc5d2
mohacelhosen <mohacel.hosen22@gmail.com>
2024-02-24 17:16:31 +0600

    Allow all origin

--- src/main/java/com/store/shop/user/controller/UserController.java
+++ src/main/java/com/store/shop/user/controller/UserController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "*"})
 @RequestMapping("/api")
 public class UserController {
     @Autowired
